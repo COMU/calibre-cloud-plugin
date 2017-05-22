166ad027b1c150a24349cc6812110fa9fb3a5abe
Kerim Ölçer <kerimlcr@gmail.com>
2017-05-23 00:31:50 +0300

    edited translation

--- main.py
+++ main.py
@@ -331,12 +331,12 @@ class AboutMainWindowForm(QDialog,AboutMainWindow):
         self.setWindowFlags(Qt.Window)
         self.label_appname.setText("Cloud Sync")
         self.label_version.setText(__version__)
-        self.label_expl.setText("Local or server files are synchronized with this plugin.")
+        self.label_expl.setText(_("Local or server files are synchronized with this plugin."))
         self.label_copy.setText('Copyright © 2017 – Kerim Ölçer\n'
     'Copyright © 2017 – Ali Güven Odabaşıoğlu')
-        self.label_cs.setText("This program comes with absolutely no warranty.\n"
-    "See the GNU General Public License, version 3 or "
-    "later for details.")
+        self.label_cs.setText(_("This program comes with absolutely no warranty.\n")
+    _("See the GNU General Public License, version 3 or ")
+    _("later for details."))
 
 
 #MainWindow
@@ -356,7 +356,7 @@ class MainWindowForm(QWidget, MainWindow):
     def openUrl(self):
         url = QtCore.QUrl('https://www.gnu.org/licenses/gpl-3.0.html')
         if not QtGui.QDesktopServices.openUrl(url):
-            QtGui.QMessageBox.warning(self, 'Open Url', 'Could not open url')
+            QtGui.QMessageBox.warning(self, 'Open Url', _('Could not open url.'))
 
     def about(self):
         self.aboutMainWindow = AboutMainWindowForm()
