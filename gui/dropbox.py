b7dc65109bcae2ca0ca535ecdd01d6269266d817
Kerim Ölçer <kerimlcr@gmail.com>
2017-05-28 00:25:44 +0300

    dropbox added

---
+++ gui/dropbox.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'yandex.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(300, 200)
+        Dialog.setMinimumSize(QtCore.QSize(300, 200))
+        Dialog.setMaximumSize(QtCore.QSize(300, 200))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman L")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        Dialog.setFont(font)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        self.downloadButton = QtWidgets.QPushButton(Dialog)
+        self.downloadButton.setGeometry(QtCore.QRect(30, 110, 101, 31))
+        self.downloadButton.setToolTip("")
+        self.downloadButton.setIconSize(QtCore.QSize(32, 32))
+        self.downloadButton.setObjectName("downloadButton")
+        self.pullButton = QtWidgets.QPushButton(Dialog)
+        self.pullButton.setGeometry(QtCore.QRect(30, 150, 101, 31))
+        self.pullButton.setToolTip("")
+        self.pullButton.setIconSize(QtCore.QSize(32, 32))
+        self.pullButton.setObjectName("pullButton")
+        self.uploadButton = QtWidgets.QPushButton(Dialog)
+        self.uploadButton.setGeometry(QtCore.QRect(170, 110, 101, 31))
+        self.uploadButton.setToolTip("")
+        self.uploadButton.setIconSize(QtCore.QSize(32, 32))
+        self.uploadButton.setObjectName("uploadButton")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(170, 150, 101, 31))
+        self.pushButton.setToolTip("")
+        self.pushButton.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton.setObjectName("pushButton")
+	self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(280, 0, 21, 21))
+        self.pushButton_2.setStyleSheet("border-color: black;\n"
+"border-width: 5px;\n"
+"border-radius: 1px;")
+        self.pushButton_2.setText("")
+	icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("images/bs_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.pushButton_2.setIcon(icon3)
+        self.pushButton_2.setIconSize(QtCore.QSize(25, 20))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.logout_button = QtWidgets.QPushButton(Dialog)
+        self.logout_button.setGeometry(QtCore.QRect(80, 50, 121, 41))
+        self.logout_button.setToolTip("")
+        self.logout_button.setIconSize(QtCore.QSize(32, 32))
+        self.logout_button.setObjectName("logout_button")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 50, 82, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman L")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.getPassword = QtWidgets.QLineEdit(Dialog)
+        self.getPassword.setGeometry(QtCore.QRect(115, 55, 171, 29))
+        self.getPassword.setObjectName("getPassword")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(21, 20, 211, 31))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman L")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.getUsername = QtWidgets.QLineEdit(Dialog)
+        self.getUsername.setGeometry(QtCore.QRect(115, 20, 171, 29))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman L")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.getUsername.setFont(font)
+        self.getUsername.setText("")
+        self.getUsername.setObjectName("getUsername")
+
+        self.retranslateUi(Dialog)
+	self.pushButton_2.clicked.connect(Dialog.close)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dropbox"))
+        self.downloadButton.setText(_translate("Dialog", "Download"))
+        self.pullButton.setText(_translate("Dialog", "Pull"))
+        self.uploadButton.setText(_translate("Dialog", "Upload"))
+        self.pushButton.setText(_translate("Dialog", "Push"))
+        self.logout_button.setText(_translate("Dialog", "Logout"))
+        self.label_2.setText(_translate("Dialog", "Password"))
+        self.label.setText(_translate("Dialog", "Username"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
